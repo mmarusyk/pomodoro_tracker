6c8ff34c972681a0bad9895eb2f870ac1cea866d
Mick <misha.marusyk@gmail.com>
2020-08-02 19:14:06 +0300

    Fix bugs and add stop button

--- js/script.js
+++ js/script.js
@@ -1,16 +1,43 @@
-function goTimer(value){
-  var time = 1499;
-  setInterval(function () {
-    if (time < 0) {
-      clearInterval();
-    } else {
-      document.getElementById("digit-minutes").textContent= Math.floor(time / 60);
-      document.getElementById("digit-seconds").textContent= time % 60;
-      time--;
-    }
-    if (time < 0) {
-      var audio = new Audio('https://onlineclock.net/audio/options/default.mp3');
-      audio.play();
-    }
-  }, 1000);
+let startButton = document.getElementById('button-start');
+let stopButton = document.getElementById('button-stop');
+let minutes = document.getElementById('digit-minutes');
+let seconds = document.getElementById('digit-seconds');
+
+let startTimer;
+
+startButton.addEventListener('click', () => {
+  startButton.disabled = true;
+  stopButton.disabled = false;
+  if (startTimer === undefined) {
+    startTimer = setInterval(goTimer, 1000)
+  }
+})
+
+stopButton.addEventListener('click', () => {
+  minutes.innerText = 25;
+  seconds.innerText = '00';
+  clearInterval(startTimer);
+  startTimer = undefined;
+  startButton.disabled = false;
+  stopButton.disabled = true;
+})
+
+function goTimer() {
+  if (seconds.innerText != 0) {
+    seconds.innerText--;
+  } else if (minutes.innerText != 0 && seconds.innerText == 0) {
+    seconds.innerText = 59;
+    minutes.innerText--;
+  }
+  if (minutes.innerText == 0 && seconds.innerText == 0) {
+    var audio = new Audio('https://onlineclock.net/audio/options/default.mp3');
+    audio.play();
+    setTimeout(() => {
+      audio.pause();
+      minutes.innerText = 25;
+      seconds.innerText = '00';
+    }, 3000);
+    clearInterval(startTimer);
+    startTimer = undefined;
+  }
 }
